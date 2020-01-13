1c66a9537ac1d3683f44411f1a0bdbf97d0d7326
Ivan Johnson <git@IvanJohnson.net>
2020-01-13 18:22:19 +0000

    Use https instead of http

--- Server/Nginx/apply.bash
+++ Server/Nginx/apply.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -euo pipefail
 cd "$(dirname "$0")"
 
 sudo mkdir -p /srv/LifeLog
 sudo cp lifelog.conf /etc/nginx/servers/
+sudo certbot --noninteractive --agree-tos --nginx --domain lifelog.ivanjohnson.net --quiet
 sudo systemctl restart nginx || (systemctl status nginx.service; exit 1)
