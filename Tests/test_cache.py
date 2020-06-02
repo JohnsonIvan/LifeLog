8314e087bd06fec69986c7768925943a7006ab83
Ivan Johnson <git@IvanJohnson.net>
2020-06-02 14:37:47 -0500

    Add multi-request cache test

--- Tests/test_cache.py
+++ Tests/test_cache.py
@@ -27,18 +27,21 @@ def _simple_test(app, headers):
         _foo()
         assert _count == initial + 1
 
+__uuid_1 = 'b6a003f7-bf11-4e9c-825a-9e4c5241a740'
+__uuid_2 = '10279e15-2fc9-4bff-b1a1-1b978fe7e7f8'
+
 @pytest.mark.unit
 def test_authenticated(app):
     headers = _default_headers.copy()
 
-    headers[LifeLogServer.cache.CACHE_HEADER] = 'b6a003f7-bf11-4e9c-825a-9e4c5241a740'
+    headers[LifeLogServer.cache.CACHE_HEADER] = __uuid_1
     _simple_test(app, headers)
 
 @pytest.mark.unit
 def test_unauthenticated(app):
     headers = _default_headers.copy()
     del headers[LifeLogServer.auth.AUTH_HEADER]
-    headers[LifeLogServer.cache.CACHE_HEADER] = '10279e15-2fc9-4bff-b1a1-1b978fe7e7f8'
+    headers[LifeLogServer.cache.CACHE_HEADER] = __uuid_1
     _simple_test(app, headers)
 
 @pytest.mark.unit
@@ -56,4 +59,25 @@ def test_noncaching(app):
         _foo()
         assert _count == initial + 3
 
-#TODO: SHOW IT CACHES MORE THAN THE MOST RECENT QUERY
+@pytest.mark.unit
+def test_multi_request(app):
+    headers = _default_headers.copy()
+
+    with app.app_context():
+        headers[LifeLogServer.cache.CACHE_HEADER] = __uuid_1
+        _simple_test(app, headers)
+
+        headers[LifeLogServer.cache.CACHE_HEADER] = __uuid_2
+        _simple_test(app, headers)
+
+        count = _count
+
+        headers[LifeLogServer.cache.CACHE_HEADER] = __uuid_1
+        with app.test_request_context(headers=headers):
+            _foo()
+            assert _count == count
+
+        headers[LifeLogServer.cache.CACHE_HEADER] = __uuid_2
+        with app.test_request_context(headers=headers):
+            _foo()
+            assert _count == count
