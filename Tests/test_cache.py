60230623254e01928106c77b4a770397f8d6c114
Ivan Johnson <git@IvanJohnson.net>
2020-06-02 14:28:03 -0500

    Split cache test in twain

--- Tests/test_cache.py
+++ Tests/test_cache.py
@@ -9,34 +9,37 @@ import LifeLogServer
 import uuid
 
 
-@pytest.mark.unit
-def test_cache(app, monkeypatch):
-    headers = {LifeLogServer.auth.AUTH_HEADER: 'asdf'}
+_count = 0
+@LifeLogServer.cache.cache
+def _foo():
+    global _count
+    _count += 1
+    return "foo"
 
-    count = 0
-    @LifeLogServer.cache.cache
-    def foo():
-        nonlocal count
-        count += 1
-        return "foo"
+_default_headers = {LifeLogServer.auth.AUTH_HEADER: 'asdf'}
+
+def _simple_test(app, headers):
+    initial = _count
+    with app.test_request_context(headers=headers):
+        _foo()
+        assert _count == initial + 1
+        _foo()
+        _foo()
+        assert _count == initial + 1
+
+@pytest.mark.unit
+def test_authenticated(app):
+    headers = _default_headers.copy()
 
     headers[LifeLogServer.cache.CACHE_HEADER] = 'b6a003f7-bf11-4e9c-825a-9e4c5241a740'
-    with app.test_request_context(headers=headers):
-        foo()
-        assert count == 1
-        foo()
-        foo()
-        assert count == 1
+    _simple_test(app, headers)
 
+@pytest.mark.unit
+def test_unauthenticated(app):
+    headers = _default_headers.copy()
     del headers[LifeLogServer.auth.AUTH_HEADER]
     headers[LifeLogServer.cache.CACHE_HEADER] = '10279e15-2fc9-4bff-b1a1-1b978fe7e7f8'
-    with app.test_request_context(headers=headers):
-        foo()
-        assert count == 2
-        foo()
-        foo()
-        assert count == 2
+    _simple_test(app, headers)
 
-    #TODO: SPLIT INTO MULTIPLE TESTS
-    #TODO: TEST W/O CACHEID
-    #TODO: SHOW IT CACHES MORE THAN THE MOST RECENT QUERY
+#TODO: TEST W/O CACHEID
+#TODO: SHOW IT CACHES MORE THAN THE MOST RECENT QUERY
