b5dacb00c15fe71c0721c632724cf7866c87b1d2
Ivan Johnson <git@IvanJohnson.net>
2020-06-12 19:28:32 -0500

    Bump verison number to 0.6.0a0.dev0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='lifelogserver',
-    version='0.5.0',
+    version='0.6.0a0.dev0',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
