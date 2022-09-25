5fa0d4f6d37a4fd01819dd353566cb6c0deefc3e
Ivan Johnson <git@IvanJohnson.net>
2022-09-25 14:48:07 -0400

    Update deploy script with new prod venv dir name

--- Scripts/deploy.bash
+++ Scripts/deploy.bash
@@ -8,20 +8,22 @@ cd $(git rev-parse --show-toplevel)
 APP_NAME="lifelogserver"
 USERNAME="lifelogserver"
 SCRIPTS_DIR="$(pwd)/Scripts/"
-ENV_DIR="$(pwd)/.venv"
+DEV_ENV_DIR="$(pwd)/.venv"
+PROD_DIR='/srv/LifeLog/Code'
+PROD_ENV_DIR_NAME="venv"
 
 # TODO: should we apply these permissions recursively? Realistically, it should
 # be sufficient to apply them to the prod directory?
 CHMOD_PERMISSIONS=750
 
-if ! [ -e "$ENV_DIR" ]; then
+if ! [ -e "$DEV_ENV_DIR" ]; then
 	echo "dev is not setup; doing now"
 	"$SCRIPTS_DIR/setup_dev.bash" || (echo SETUP_DEV SCRIPT FAILED; exit 1)
 fi
 
 echo activating venv
 set +euo pipefail
-source "${ENV_DIR}/bin/activate"
+source "${DEV_ENV_DIR}/bin/activate"
 set -euo pipefail
 
 pip install wheel
@@ -34,7 +36,6 @@ python setup.py bdist_wheel
 id -u "$USERNAME" || (echo creating the user "\"${USERNAME}\""; sudo useradd -r -s /usr/bin/nologin "$USERNAME")
 
 # Create top-level production directory
-PROD_DIR='/srv/LifeLog/Code'
 sudo mkdir -p "$PROD_DIR"
 sudo chown "$USERNAME:$USERNAME" "/srv/LifeLog"
 sudo chmod "$CHMOD_PERMISSIONS" "/srv/LifeLog"
@@ -59,17 +60,17 @@ Launch LifeLogServer, wait until loaded then stop it:
 	sudo systemctl stop lifelogserver
 Purge/reinitialize the database if necessary (automatic migrations have not yet been implemented, as of 2022-09-19):
 	purge:
-		sudo trash "${PROD_DIR}/.venv/var/LifeLogServer-instance/lifelog.sqlite"
+		sudo trash "${PROD_DIR}/${PROD_ENV_DIR_NAME}/var/LifeLogServer-instance/lifelog.sqlite"
 	Restore backup:
 		# UNTESTED
-		sudo install --owner "$USERNAME" --group "$USERNAME" -d ./path/to/backup/venv_var_LifeLogServer-instance.bak -t "${PROD_DIR}/.venv/var/LifeLogServer-instance"
+		sudo install --owner "$USERNAME" --group "$USERNAME" -d ./path/to/backup/${PROD_ENV_DIR_NAME}_var_LifeLogServer-instance.bak -t "${PROD_DIR}/${PROD_ENV_DIR_NAME}/var/LifeLogServer-instance"
 	(Re-)initialize:
-		sudo -u "$USERNAME" bash -c 'cd "$PROD_DIR"; source ./.venv/bin/activate; export FLASK_APP=LifeLogServer; flask init-db'
+		sudo -u "$USERNAME" bash -c 'cd "$PROD_DIR"; source ./${PROD_ENV_DIR_NAME}/bin/activate; export FLASK_APP=LifeLogServer; flask init-db'
 Set a secret key for doing secure stuff like signing session:
 	sudo -u "$USERNAME" bash << DELIMITER
-	mkdir -p "${PROD_DIR}/.venv/var/flaskr-instance/"
-	python -c 'import os; print(f"SECRET_KEY = {os.urandom(16)}")' > /srv/LifeLog/Code/.venv/var/flaskr-instance/config.py
-	chmod 600 /srv/LifeLog/Code/.venv/var/flaskr-instance/config.py
+	mkdir -p "${PROD_DIR}/${PROD_ENV_DIR_NAME}/var/flaskr-instance/"
+	python -c 'import os; print(f"SECRET_KEY = {os.urandom(16)}")' > ${PROD_DIR}/${PROD_ENV_DIR_NAME}/var/flaskr-instance/config.py
+	chmod 600 ${PROD_DIR}/${PROD_ENV_DIR_NAME}/var/flaskr-instance/config.py
 	DELIMITER
 Enable/start LifeLogServer:
 	sudo systemctl enable --now "$APP_NAME"
