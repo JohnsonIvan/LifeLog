319931bfcd92030d7d77c65e1dbb6cd9ef6d55ea
Ivan Johnson <git@IvanJohnson.net>
2022-09-25 16:15:13 -0400

    Create placeholder website

--- Scripts/deploy.bash
+++ Scripts/deploy.bash
@@ -7,9 +7,11 @@ cd $(git rev-parse --show-toplevel)
 
 APP_NAME="lifelogserver"
 USERNAME="lifelogserver"
-SCRIPTS_DIR="$(pwd)/Scripts/"
+SCRIPTS_DIR="$(pwd)/Scripts"
 DEV_ENV_DIR="$(pwd)/.venv"
-PROD_DIR='/srv/LifeLog/Code'
+WEBSTATIC_DIR="$(pwd)/WebStatic"
+PROD_DIR='/srv/LifeLog'
+CODE_DIR='${PROD_DIR}/Code' #TODO: needs a better name. Backend?
 PROD_ENV_DIR_NAME="venv"
 CHMOD_PERMISSIONS=750
 
@@ -32,19 +34,15 @@ python setup.py bdist_wheel
 
 id -u "$USERNAME" || (echo creating the user "\"${USERNAME}\""; sudo useradd -r -s /usr/bin/nologin "$USERNAME")
 
-# Create top-level production directory
-sudo mkdir -p "$PROD_DIR"
-sudo chown "$USERNAME:$USERNAME" "/srv/LifeLog"
-sudo chmod "$CHMOD_PERMISSIONS" "/srv/LifeLog"
-sudo chown "$USERNAME:$USERNAME" "$PROD_DIR"
-sudo chmod "$CHMOD_PERMISSIONS" "$PROD_DIR"
+sudo rm -f "$CODE_DIR/$APP_NAME-"*
 
-sudo rm -f "$PROD_DIR/$APP_NAME-"*
+# ~sudo cp $WEBSTATIC_DIR $PROD_DIR
+(cd "${WEBSTATIC_DIR}"; find . -type f -exec sudo install -D "--mode=${CHMOD_PERMISSIONS}" "{}" "${PROD_DIR}/WebStatic/{}" \;)
 
-sudo install "--mode=${CHMOD_PERMISSIONS}" "dist/$APP_NAME-"*".whl"                    "$PROD_DIR"
-sudo install "--mode=${CHMOD_PERMISSIONS}" "$SCRIPTS_DIR/launch.bash"                  "$PROD_DIR"
-sudo install "--mode=${CHMOD_PERMISSIONS}" "$SCRIPTS_DIR/lifelogserver.service"        "/etc/systemd/system/"
-sudo install "--mode=${CHMOD_PERMISSIONS}" "$SCRIPTS_DIR/net.ivanjohnson.lifelog.conf" "/etc/nginx/servers/"
+sudo install -D "--mode=${CHMOD_PERMISSIONS}"             "dist/$APP_NAME-"*".whl"                    "$CODE_DIR" # TODO: is it possible for there to be multiple versions?
+sudo install -D "--mode=${CHMOD_PERMISSIONS}"             "$SCRIPTS_DIR/launch.bash"                  "$CODE_DIR"
+sudo install -D "--mode=${CHMOD_PERMISSIONS}"             "$SCRIPTS_DIR/lifelogserver.service"        "/etc/systemd/system/"
+sudo install -D "--mode=${CHMOD_PERMISSIONS}"             "$SCRIPTS_DIR/net.ivanjohnson.lifelog.conf" "/etc/nginx/servers/"
 
 sudo systemctl daemon-reload
 sudo systemctl restart lifelogserver
@@ -57,17 +55,17 @@ Launch LifeLogServer, wait until loaded then stop it:
 	sudo systemctl stop lifelogserver
 Purge/reinitialize the database if necessary (automatic migrations have not yet been implemented, as of 2022-09-19):
 	purge:
-		sudo trash "${PROD_DIR}/${PROD_ENV_DIR_NAME}/var/LifeLogServer-instance/lifelog.sqlite"
+		sudo trash "${CODE_DIR}/${PROD_ENV_DIR_NAME}/var/LifeLogServer-instance/lifelog.sqlite"
 	Restore backup:
 		# UNTESTED
-		sudo install --owner "$USERNAME" --group "$USERNAME" -d ./path/to/backup/${PROD_ENV_DIR_NAME}_var_LifeLogServer-instance.bak -t "${PROD_DIR}/${PROD_ENV_DIR_NAME}/var/LifeLogServer-instance"
+		sudo install --owner "$USERNAME" --group "$USERNAME" -d ./path/to/backup/${PROD_ENV_DIR_NAME}_var_LifeLogServer-instance.bak -t "${CODE_DIR}/${PROD_ENV_DIR_NAME}/var/LifeLogServer-instance"
 	(Re-)initialize:
-		sudo -u "$USERNAME" bash -c 'cd "$PROD_DIR"; source ./${PROD_ENV_DIR_NAME}/bin/activate; export FLASK_APP=LifeLogServer; flask init-db'
+		sudo -u "$USERNAME" bash -c 'cd "$CODE_DIR"; source ./${PROD_ENV_DIR_NAME}/bin/activate; export FLASK_APP=LifeLogServer; flask init-db'
 Set a secret key for doing secure stuff like signing session:
 	sudo -u "$USERNAME" bash << DELIMITER
-	mkdir -p "${PROD_DIR}/${PROD_ENV_DIR_NAME}/var/flaskr-instance/"
-	python -c 'import os; print(f"SECRET_KEY = {os.urandom(16)}")' > ${PROD_DIR}/${PROD_ENV_DIR_NAME}/var/flaskr-instance/config.py
-	chmod 600 ${PROD_DIR}/${PROD_ENV_DIR_NAME}/var/flaskr-instance/config.py
+	mkdir -p "${CODE_DIR}/${PROD_ENV_DIR_NAME}/var/flaskr-instance/"
+	python -c 'import os; print(f"SECRET_KEY = {os.urandom(16)}")' > ${CODE_DIR}/${PROD_ENV_DIR_NAME}/var/flaskr-instance/config.py
+	chmod 600 ${CODE_DIR}/${PROD_ENV_DIR_NAME}/var/flaskr-instance/config.py
 	DELIMITER
 Enable/start LifeLogServer:
 	sudo systemctl enable --now "$APP_NAME"
