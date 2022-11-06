f2b4ee2e9dcf2e03210700df71d69be51d8635e4
Ivan Johnson <git@IvanJohnson.net>
2022-11-06 14:15:01 -0500

    Fix connection between nginx and waitress
    I know that the immediate issue was that on my server nginx was trying to use
    ipv6 to communicate with waitress, while waitress was only trying to use ipv4. I
    would have expected Waitress to support ipv6 by default. I saw some weird errors
    when I had waitress disable ipv4 and explicitly enable ipv6. I don't know why
    using `--listen` instead of `--no-ipv4 --ipv6` works.

--- Scripts/launch.bash
+++ Scripts/launch.bash
@@ -19,4 +19,4 @@ source "${ENV_DIR}/bin/activate"
 echo "Installing LLS in the venv"
 pip install "${PKG_NAME}-"*".whl"
 
-waitress-serve --port 8080 --call "${APP_NAME}:create_app"
+waitress-serve --listen=[::1]:8080 --call "${APP_NAME}:create_app"
