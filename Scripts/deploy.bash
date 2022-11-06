28b0dcc040d8ad5484331509d6372be27507a118
Ivan Johnson <git@IvanJohnson.net>
2022-11-06 13:20:23 -0500

    Fix for launch script not being executable

--- Scripts/deploy.bash
+++ Scripts/deploy.bash
@@ -6,7 +6,8 @@ cd "$DIR"
 cd $(git rev-parse --show-toplevel)
 
 APP_NAME="lifelogserver"
-USERNAME="lifelogserver"
+USER="lifelogserver"
+GROUP="$USER"
 SCRIPTS_DIR="$(pwd)/Scripts"
 DEV_ENV_DIR="$(pwd)/.venv"
 WEBSTATIC_DIR="$(pwd)/WebStatic"
@@ -32,17 +33,17 @@ git clean -di
 
 python setup.py bdist_wheel
 
-id -u "$USERNAME" || (echo creating the user "\"${USERNAME}\""; sudo useradd -r -s /usr/bin/nologin "$USERNAME")
+id -u "$USER" || (echo creating the user "\"${USER}\""; sudo useradd -r -s /usr/bin/nologin "$USER")
 
 sudo rm -f "$CODE_DIR/$APP_NAME-"*
 
 # ~sudo cp $WEBSTATIC_DIR $PROD_DIR
 (cd "${WEBSTATIC_DIR}"; find . -type f -exec sudo install -D "--mode=${CHMOD_PERMISSIONS}" "{}" "${PROD_DIR}/WebStatic/{}" \;)
 
-sudo install -D "--mode=${CHMOD_PERMISSIONS}"             "dist/$APP_NAME-"*".whl"                    "$CODE_DIR" # TODO: is it possible for there to be multiple versions?
-sudo install -D "--mode=${CHMOD_PERMISSIONS}"             "$SCRIPTS_DIR/launch.bash"                  "$CODE_DIR"
-sudo install -D "--mode=${CHMOD_PERMISSIONS}"             "$SCRIPTS_DIR/lifelogserver.service"        "/etc/systemd/system/"
-sudo install -D "--mode=${CHMOD_PERMISSIONS}"             "$SCRIPTS_DIR/net.ivanjohnson.lifelog.conf" "/etc/nginx/servers/"
+sudo install -D "--mode=${CHMOD_PERMISSIONS}" --owner "root" --group "$GROUP" "dist/$APP_NAME-"*".whl"                    "$CODE_DIR" # TODO: is it possible for there to be multiple versions?
+sudo install -D "--mode=${CHMOD_PERMISSIONS}" --owner "root" --group "$GROUP" "$SCRIPTS_DIR/launch.bash"                  "$CODE_DIR"
+sudo install -D "--mode=${CHMOD_PERMISSIONS}" --owner "root" --group "$GROUP" "$SCRIPTS_DIR/lifelogserver.service"        "/etc/systemd/system/"
+sudo install -D "--mode=${CHMOD_PERMISSIONS}" --owner "root" --group "$GROUP" "$SCRIPTS_DIR/net.ivanjohnson.lifelog.conf" "/etc/nginx/servers/"
 
 sudo systemctl daemon-reload
 sudo systemctl restart lifelogserver
@@ -58,11 +59,11 @@ Purge/reinitialize the database if necessary (automatic migrations have not yet 
 		sudo trash "${CODE_DIR}/${PROD_ENV_DIR_NAME}/var/LifeLogServer-instance/lifelog.sqlite"
 	Restore backup:
 		# UNTESTED
-		sudo install --owner "$USERNAME" --group "$USERNAME" -d ./path/to/backup/${PROD_ENV_DIR_NAME}_var_LifeLogServer-instance.bak -t "${CODE_DIR}/${PROD_ENV_DIR_NAME}/var/LifeLogServer-instance"
+		sudo install --owner "$USER" --group "$GROUP" -d ./path/to/backup/${PROD_ENV_DIR_NAME}_var_LifeLogServer-instance.bak -t "${CODE_DIR}/${PROD_ENV_DIR_NAME}/var/LifeLogServer-instance"
 	(Re-)initialize:
-		sudo -u "$USERNAME" bash -c 'cd "$CODE_DIR"; source ./${PROD_ENV_DIR_NAME}/bin/activate; export FLASK_APP=LifeLogServer; flask init-db'
+		sudo -u "$USER" bash -c 'cd "$CODE_DIR"; source ./${PROD_ENV_DIR_NAME}/bin/activate; export FLASK_APP=LifeLogServer; flask init-db'
 Set a secret key for doing secure stuff like signing session:
-	sudo -u "$USERNAME" bash << DELIMITER
+	sudo -u "$USER" bash << DELIMITER
 	mkdir -p "${CODE_DIR}/${PROD_ENV_DIR_NAME}/var/flaskr-instance/"
 	python -c 'import os; print(f"SECRET_KEY = {os.urandom(16)}")' > ${CODE_DIR}/${PROD_ENV_DIR_NAME}/var/flaskr-instance/config.py
 	chmod 600 ${CODE_DIR}/${PROD_ENV_DIR_NAME}/var/flaskr-instance/config.py
