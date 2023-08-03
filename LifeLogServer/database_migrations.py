566c9b51839024deef1ed91411d0dc733f1fa82e
Ivan Johnson <git@IvanJohnson.net>
2023-08-03 12:16:51 -0400

    Add the '0.7.0a0.dev2' -> '0.7.0' database migration

--- LifeLogServer/database_migrations.py
+++ LifeLogServer/database_migrations.py
@@ -5,6 +5,10 @@ def migrate_0_7_0a0_dev0_to_dev1(db):
     print("RUNNING MIGRATION FOR 0.7.0a0.dev0 -> 0.7.0a0.dev1")
     db.execute('ALTER TABLE weight RENAME COLUMN weight to weight_kg')
 
+@database.migration('0.7.0a0.dev2', '0.7.0')
+def migrate_0_7_0a0_dev2_to_0_7_0(db): # pragma: no cover
+    pass
+
 #SQLite apparently doesn't support the addition of foreign key constraints
 #(https://stackoverflow.com/a/1884841) to existing tables, and there aren't
 #enough users of v0.7.0a0.dev1 to justify working around this issue with SQL
