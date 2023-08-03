bca3dca116af9aeb8505e40b28f3d1191918b252
Ivan Johnson <git@IvanJohnson.net>
2023-08-03 12:11:24 -0400

    Release v0.7.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='lifelogserver',
-    version='0.7.0a0.dev2',
+    version='0.7.0',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
