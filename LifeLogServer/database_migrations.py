574539eba2d224585b51a1abb7a9d6e69165d44b
Ivan Johnson <git@IvanJohnson.net>
2023-08-03 12:20:48 -0400

    Fix new DB migration script

--- LifeLogServer/database_migrations.py
+++ LifeLogServer/database_migrations.py
@@ -7,7 +7,7 @@ def migrate_0_7_0a0_dev0_to_dev1(db):
 
 @database.migration('0.7.0a0.dev2', '0.7.0')
 def migrate_0_7_0a0_dev2_to_0_7_0(db): # pragma: no cover
-    pass
+    db.execute('CREATE TABLE weight_goal (   userid TEXT NOT NULL,   datetime_start INTEGER NOT NULL,   weight_kg_start REAL NOT NULL,   weight_change_kg_per_year REAL NOT NULL,   FOREIGN KEY(userid) REFERENCES users(userid),   PRIMARY KEY (userid, datetime_start) )')
 
 #SQLite apparently doesn't support the addition of foreign key constraints
 #(https://stackoverflow.com/a/1884841) to existing tables, and there aren't
