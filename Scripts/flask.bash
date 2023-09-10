67a7d8389b93e30e433eb4a195e834360e99196b
Ivan Johnson <git@IvanJohnson.net>
2023-09-10 10:38:53 -0400

    Bugfix

--- Scripts/flask.bash
+++ Scripts/flask.bash
@@ -34,12 +34,12 @@ if [ "${1:-}" = "reinit-dev" ] ; then
 	"$this" dev-db "INSERT INTO tokens (token, userid, description) VALUES ('$KEY', '$USER_ID', 'Dev key')"
 	"$this" dev-db "INSERT INTO token_perms (token, permission) VALUES ('$KEY', 'ultimate')"
 
-	"$this" dev-db "INSERT INTO weight (id, userid, datetime, weight_kg) VALUES ('fec46a2e-cf9c-4dea-b5ef-5bc1aef34d8a', '$USER_ID', 1450483200, 70.5)"
-	"$this" dev-db "INSERT INTO weight (id, userid, datetime, weight_kg) VALUES ('9c4cfa98-2c09-4897-a0af-ab0503c6af72', '$USER_ID', 1450656000, 69.9)"
-	"$this" dev-db "INSERT INTO weight (id, userid, datetime, weight_kg) VALUES ('20db843d-b349-4589-8e8d-7e4b2d3d588d', '$USER_ID', 1460934000, 73.0)"
-	"$this" dev-db "INSERT INTO weight (id, userid, datetime, weight_kg) VALUES ('932bd88c-9184-46b9-b59b-e4c0b0bef214', '$USER_ID', 1466377200, 69.2)"
-
-	"$this" dev-db "INSERT INTO weight_goal (userid, datetime_start, weight_kg_start, weight_change_kg_per_year) VALUES ('$USER_ID', 1450483000, 71.0, -6)"
+	"$this" dev-db "INSERT INTO weight_goal (userid, datetime_start, weight_kg_start, weight_change_kg_per_year) VALUES ('$USER_ID', 1450396800, 70.0, 60)" # 2015-12-18T00:00:00Z
+	"$this" dev-db "INSERT INTO weight (id, userid, datetime, weight_kg) VALUES ('fec46a2e-cf9c-4dea-b5ef-5bc1aef34d8a', '$USER_ID', 1450483200, 70.5)"      # 2015-12-19T00:00:00Z
+	"$this" dev-db "INSERT INTO weight (id, userid, datetime, weight_kg) VALUES ('9c4cfa98-2c09-4897-a0af-ab0503c6af72', '$USER_ID', 1450656000, 69.9)"      # 2015-12-21T00:00:00Z
+	"$this" dev-db "INSERT INTO weight_goal (userid, datetime_start, weight_kg_start, weight_change_kg_per_year) VALUES ('$USER_ID', 1451606400, 71.0, -6)"	 # 2016-01-01T00:00:00Z
+	"$this" dev-db "INSERT INTO weight (id, userid, datetime, weight_kg) VALUES ('20db843d-b349-4589-8e8d-7e4b2d3d588d', '$USER_ID', 1452988800, 73.0)"      # 2016-01-17T00:00:00Z
+	"$this" dev-db "INSERT INTO weight (id, userid, datetime, weight_kg) VALUES ('932bd88c-9184-46b9-b59b-e4c0b0bef214', '$USER_ID', 1455840000, 69.2)"      # 2016-02-19T00:00:00Z
 elif [ "${1:-}" = "dev-db" ] ; then
 	shift
 	sqlite3 "${DEV_DB}" "$@"
