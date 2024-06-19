6fdeaf2b546b25df75483ac16f7eb2358f0da1da
Ivan Johnson <git@IvanJohnson.net>
2024-06-19 17:24:45 -0400

    Cherry-pick "Add missing DB migrations"
    This is a cherry-pick of 566688c38b1eb96d3e7b409e2a61166cb859db93

--- src/LifeLogServer/database_migrations.py
+++ src/LifeLogServer/database_migrations.py
@@ -14,6 +14,13 @@ def migrate_0_7_0a0_dev2_to_0_7_0(db):  # pragma: no cover
     )
 
 
+
+@database.migration("0.7.0", "0.7.2")
+@database.migration("0.7.1", "0.7.2")
+def migrate_0_7_X_to_0_7_2(db):  # pragma: no cover
+    pass
+
+
 # SQLite apparently doesn't support the addition of foreign key constraints
 # (https://stackoverflow.com/a/1884841) to existing tables, and there aren't
 # enough users of v0.7.0a0.dev1 to justify working around this issue with SQL
