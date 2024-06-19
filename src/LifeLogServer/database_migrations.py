b7c582060045e2b1e5a0e8cac31adbc2208965df
Ivan Johnson <git@IvanJohnson.net>
2024-06-19 17:34:30 -0400

    Implement database migrations

--- src/LifeLogServer/database_migrations.py
+++ src/LifeLogServer/database_migrations.py
@@ -1,5 +1,11 @@
 from . import database
 
+@database.migration("0.7.0", "0.7.2")
+@database.migration("0.7.1", "0.7.2")
+@database.migration("0.7.2", "0.8.0a0.dev0")
+def migrate_noop(db):  # pragma: no cover
+    pass
+
 
 @database.migration("0.7.0a0.dev0", "0.7.0a0.dev1")
 def migrate_0_7_0a0_dev0_to_dev1(db):
@@ -26,7 +32,7 @@ def migrate_0_7_0a0_dev2_to_0_7_0(db):  # pragma: no cover
     )
 
 
-@database.migration("0.7.0", "0.7.2")
-@database.migration("0.7.1", "0.7.2")
-def migrate_0_7_X_to_0_7_2(db):  # pragma: no cover
-    pass
+@database.migration("0.7.0a0.dev2", "0.8.0a0.dev0")
+def migrate_0_7_0a0_dev2_to_0_8_0a0_dev0(db):  # pragma: no cover
+    migrate_0_7_0a0_dev2_to_0_7_0(db)
+    # Updating from 0.7.0 -> 0.8.0a0.dev0 is a no-op
